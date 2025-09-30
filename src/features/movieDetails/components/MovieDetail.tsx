eaccf0c56a0c41d3a4420f145fefb32a5a8ce334
isleyva <deepman42@protonmail.com>
2025-09-30 20:29:37 +0200

    Enhance MovieDetail component: add no recommendations message and style; remove MovieCarousel component

--- src/features/movieDetails/components/MovieDetail.tsx
+++ src/features/movieDetails/components/MovieDetail.tsx
@@ -98,15 +98,29 @@ export const MovieDetail = memo<MovieDetailProps>(({ movie }) => {
           </div>
         </div>
 
-        {movie.recommendations && movie.recommendations.length > 0 && (
-          <div className="movie-detail__recommendations">
+        <div className="movie-detail__recommendations">
+          {movie.recommendations && movie.recommendations.length > 0 ? (
             <MovieCarousel
               title="YOU MIGHT ALSO LIKE"
               movies={movie.recommendations}
               isLoading={false}
             />
-          </div>
-        )}
+          ) : (
+            <div className="movie-detail__no-recommendations">
+              <div className="movie-detail__no-recommendations-content">
+                <h3 className="movie-detail__no-recommendations-title">
+                  RECOMMENDATIONS
+                </h3>
+                <p className="movie-detail__no-recommendations-message">
+                  We don't have recommendations for this movie yet.
+                </p>
+                <p className="movie-detail__no-recommendations-subtitle">
+                  Check back later for curated suggestions.
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
